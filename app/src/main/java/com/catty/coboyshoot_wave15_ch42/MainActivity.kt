4a82d31f2149d1aca02c0bfd161fe7200076a337
yordawira <50675414+yordawira@users.noreply.github.com>
2022-10-10 21:24:26 +0700

    Initial commit

---
+++ app/src/main/java/com/catty/coboyshoot_wave15_ch42/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.catty.coboyshoot_wave15_ch42
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
